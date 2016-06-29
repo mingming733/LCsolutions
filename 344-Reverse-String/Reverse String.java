e71aa119dc96dfd60543b98966baacb295fc48c9
mingming733 <jingyang.ming@gmail.com>
2016-06-29 11:05:55 -0700

    [344. Reverse String][Accepted]committed by LeetCode Extension

---
+++ 344-Reverse-String/Reverse String.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public String reverseString(String s) {
+        StringBuffer sb = new StringBuffer();
+        for(int i = s.length() - 1; i >= 0; i--){
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
